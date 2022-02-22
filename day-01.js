a2ecb59457cbdc38c4b3d98c3763f8bdc5893601
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-02-22 13:03:25 +0200

    Create day-01.js

---
+++ day-01.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+
+// Training JS #1: create your first JS function and print "Helloworld!"
+
+function  helloWorld() {
+  const str = 'Hello World!';
+  console.log(str);
+}

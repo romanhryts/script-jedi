f507a95960e45ed62381056f24ca8c1981776e90
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-02-22 13:58:51 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -2,7 +2,12 @@
 
 // Training JS #2: Basic data types--Number
 
-let v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250;
+let v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
 
 const equal1 = () => v1 + v1;
 const equal2 = () => v3 - v1;
@@ -30,7 +35,6 @@ const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
 const answer1 = () => "no";
 const answer2 = () => "no";
 const answer3 = () => "yes";
-
 
 /************************************************************************/
 

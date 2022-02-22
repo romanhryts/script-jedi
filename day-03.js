48824220a071ead0585d7ee101056c44709c1f54
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-02-22 13:56:34 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -11,3 +11,36 @@ const trueOrFalse = (val) => {
     ? "false"
     : "true";
 };
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+
+// Training JS #7: if..else and ternary operator
+
+const saleHotdogs = (n) =>
+  n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript
+
+// Training JS #8: Conditional statement--switch
+
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
+      break;
+    default:
+      days = 31;
+  }
+  return days;
+}

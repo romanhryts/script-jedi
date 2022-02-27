a5035c859444119a2ccc1ad56e462df0dadf657e
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-02-27 19:08:30 +0200

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -12,3 +12,21 @@ function maxMultiple(divisor, bound) {
   }
   return result;
 }
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+// Is he gonna survive?
+
+function hero(bullets, dragons) {
+  return bullets / dragons >= 2 ? true : false;
+}
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+// 5 without numbers !!
+
+const unusualFive = () => "World".length;

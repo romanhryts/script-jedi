dcfa5fe8215e0854eeb3d64839d7fc4a03ce679f
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-03-01 20:13:07 +0200

    Create day-09.js

---
+++ day-09.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+// Training JS #16: Methods of String object--slice(), substring() and substr()
+
+function cutIt(arr) {
+  let result;
+  for (let str of arr) {
+    let minLength = Math.min(...arr.map((str) => str.length));
+    result = arr.map((str) => str.slice(0, minLength));
+  }
+  return result;
+}
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
+
+// Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()
+
+function firstToLast(str, c) {
+  const firstSymbol = str.indexOf(c);
+  const lastSymbol = str.lastIndexOf(c);
+  let position;
+  return str.indexOf(c) === -1
+    ? (position = -1)
+    : (position = lastSymbol - firstSymbol);
+}
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/57280481e8118511f7000ffa/train/javascript
+
+// Training JS #18: Methods of String object--concat() split() and its good friend join()
+
+function splitAndMerge(string, separator) {
+  const words = string.split(' ')
+  const letters = words.map(word => word.split('').join(separator));
+  const improvedStr = letters.join(' ');
+  return improvedStr;
+}

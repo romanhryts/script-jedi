be35ba4ddc94c726f87d8e4bd6c5f09a195d0774
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-03-01 20:59:36 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -5,3 +5,7 @@
 function alienLanguage(str) {
  return str.split(' ').map(word => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase()).join(' ');
 }
+
+/************************************************************************/
+
+//

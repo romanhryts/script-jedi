5ce1b3503bce54862ff3ddd932d24132a682fdbe
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-03-01 20:52:53 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -1,1 +1,7 @@
+// https://www.codewars.com/kata/5728203b7fc662a4c4000ef3/train/javascript
 
+// Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()
+
+function alienLanguage(str) {
+ return str.split(' ').map(word => word.slice(0, -1).toUpperCase() + word.slice(-1).toLowerCase()).join(' ');
+}

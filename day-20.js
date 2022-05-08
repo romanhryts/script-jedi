23fb8841ea870dc079fe34276d59631162859043
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-05-08 19:45:17 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+
+// Training JS #39: Regular Expression--"?", "*", "+" and "{}"
+
+const regex = /^-?9\d*0{4}$/;
+
+/************************************************************************/
+
+// https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
+
+// Training JS #40: Regular Expression--"|", "[]" and "()"
+
+const regex = /https?:\/\/[a-z\d.]+\.(com|net)/gi;

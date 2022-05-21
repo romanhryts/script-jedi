fb7a7cae6a4a688a48ab396a335571455f88551e
romanhryts <96837069+romanhryts@users.noreply.github.com>
2022-05-21 14:12:57 +0200

    Create day-30.js

---
+++ day-30.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/52b50a20fa0e77b304000103/train/javascript
+
+// SantaClausable Interface
+
+function isSantaClausable(obj) {
+  const methods = ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'];
+  return methods.every(method => typeof obj[method] === 'function');
+}
+
+/***********************************************************************/
+
+// https://www.codewars.com/kata/52e5e86b2ff32887280002e4/train/javascript
+
+// Cylon Evolution
+
+class Cylon {
+  constructor(model) {
+    this.model = model;
+    this.attack = () => "Destroy all humans!";
+  }
+}
+
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model) 
+      this.model = model;
+      this.infiltrate = () => "Infiltrate the colonies";
+  }
+}
